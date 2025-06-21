7561d7ea4991fd3032f072e0221c20a173bf10d9
Caio Fiszuk <caiolopes871@gmail.com>
2025-06-21 20:06:21 -0300

    Cria o conceito da página inicial

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+
+function App() {
+
+  return (
+    <div>
+       <section className="circles">
+        <div className="tier">
+          <div className="circle">  </div>
+        </div>
+        <div className="tier">
+          <div className="circle">  </div>
+          <div className="circle">  </div>
+        </div>
+        <div className="tier">
+          <div className="circle">  </div>
+          <div className="circle">  </div>
+          <div className="circle">  </div>
+        </div>
+        <div className="tier">
+          <div className="circle">  </div>
+          <div className="circle">  </div>
+          <div className="circle">  </div>
+          <div className="circle">  </div>
+        </div>
+       </section>
+    </div>
+  )
+}
+
+export default App;
